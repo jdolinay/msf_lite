ef6001e3099a819c263c98f8f5109d4bba1385f9
dolinay <dolinay@fai.utb.cz>
2014-06-23 13:48:20 +0200

    Template for msf_config.h updated with new settings (timer pins etc.)

--- template/cw/Project_Headers/msf_config.h
+++ template/cw/Project_Headers/msf_config.h
@@ -31,6 +31,77 @@
    	   	 Core clock = 20.9MHz, BusClock = ?
 */
 
+/*********************************************
+*    Define the standard I/O channel 
+********************************************/
+#define   MSF_USE_STDIO     1   /* Using conio driver (UART) for std output */
+/* To disable initialising UART in msf_init use:
+#define   MSF_USE_STDIO     0 
+*/
+/* Define the default baudrate used by console I/O (conio).
+ * This must be one of the values from the enum defined in msf_<device>.h. */
+#define	 MSF_STDIO_BAUDRATE		(BD19200)
+
+/*********************************************
+*    Define whether we want to use analog inputs
+*    If nonzero; ADC is initialised. This means 
+*    little more power consumption, otherwise no
+*    harm is done by enabling the analog inputs even
+*    if not used 
+********************************************/
+#define MSF_USE_ANALOG      0
+
+/*********************************************
+*    Define which drivers should be created (used) 
+*    It may save some memory to define drivers which 
+*    your applicaiton will not use with 0.
+********************************************/
+#define MSF_DRIVER_UART0    1   /* Use UART0 driver (create UART0 driver instance) */
+#define MSF_DRIVER_ADC0     0	/* Use ADC driver */
+#define	MSF_DRIVER_TPM0		1	/* Use TPM0 driver */
+#define	MSF_DRIVER_TPM1		1	/* Use TPM1 driver */
+#define	MSF_DRIVER_TPM2		1	/* Use TPM2 driver */
+
+
+/*********************************************
+* Pin configuration for the timer TPMn drivers.
+* Here you can select which pin is used for each channel from
+* the available pins for this channel.
+* You will need to look into the datasheet into the Pinout chapter
+* (in Signal Multiplexing and Signal Descriptions) to find out which
+* pins are available for each channel.
+* Define the name of the pin as defined in msf_<device>.h for you MCU
+* and the number of the ALTx function as found in the pinout table in 
+* the datasheet.
+**********************************************/
+
+/* Timer TPM0 pins (6 channels are available) */
+#define		MSF_TPM0_CH0_PIN	(GPIO_A3)
+#define		MSF_TPM0_CH0_ALT	(3)
+#define		MSF_TPM0_CH1_PIN	(GPIO_D1)
+#define		MSF_TPM0_CH1_ALT	(4)
+#define		MSF_TPM0_CH2_PIN	(GPIO_A5)
+#define		MSF_TPM0_CH2_ALT	(3)
+#define		MSF_TPM0_CH3_PIN	(GPIO_E30)
+#define		MSF_TPM0_CH3_ALT	(3)
+#define		MSF_TPM0_CH4_PIN	(GPIO_E31)
+#define		MSF_TPM0_CH4_ALT	(3)
+#define		MSF_TPM0_CH5_PIN	(GPIO_A0)
+#define		MSF_TPM0_CH5_ALT	(3)
+
+/* Timer TPM1 pins (only 2 channels on KL25) */
+#define		MSF_TPM1_CH0_PIN	(GPIO_E20)	/* or PTA12/3; PTB0/3*/
+#define		MSF_TPM1_CH0_ALT	(3)
+#define		MSF_TPM1_CH1_PIN	(GPIO_E21)	/* or PTA13/3; PTB1/3*/
+#define		MSF_TPM1_CH1_ALT	(3)
+
+/* Timer TPM2 pins (only 2 channels on KL25) */
+#define		MSF_TPM2_CH0_PIN	(GPIO_B18)	/* or PTA1/3; PTB2/3; PTE22/3*/
+#define		MSF_TPM2_CH0_ALT	(3)
+#define		MSF_TPM2_CH1_PIN	(GPIO_B19)  /* or PTA2/3; PTB2/3; PTE22/3*/
+#define		MSF_TPM2_CH1_ALT	(3)
+
+
  /* Include the header file for our board */
 #include "frdm_kl25z.h" 
 
